6ee36c5a6ac47bf336e49f32bc594a19a2eda566
codename-lita <lita.1615@gmail.com>
2023-05-22 02:23:06 -0500

    finalized CSS HTML and JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+// function for Day and Time
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -20,7 +22,7 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-
+//function to display weather details
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -45,18 +47,38 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.icon);
 }
 
+//function to search for a city
 function search(city) {
   let apiKey = "0be4d0ade7d8f64fb3ca53dd30f6fotd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   https: axios.get(apiUrl).then(displayTemperature);
 }
-
+// function to search city button
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
+// function for current location
+function currentPosition(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+  let apiKey = "0be4d0ade7d8f64fb3ca53dd30f6fotd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+  https: axios.get(apiUrl).then(displayTemperature);
+}
+//function for current location button
+function searchCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+let currentLocationBtn = document.querySelector(".current-location-btn");
+currentLocationBtn.addEventListener("click", searchCurrentLocation);
+
+//functions to covert and display celcius and fahrenheit temp
 function displayFarenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -77,13 +99,12 @@ function displaycelciusTemperature(event) {
 
 let celciusTemperature = null;
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
+//function for temp unit links click
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFarenheitTemperature);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displaycelciusTemperature);
 
+//default city 
 search("New York");
